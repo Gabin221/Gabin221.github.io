a97b6b911259f3b82fc2d739d2d2d91641155d9f
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-02-13 14:18:09 +0100

    Delete boutonPanier.js

--- boutonPanier.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('boutonListeCommande').addEventListener('click', function() {
-    var tooltip = document.getElementById('divListeCommande');
-    tooltip.style.display = (tooltip.style.display === 'none') ? 'block' : 'none';
-});
