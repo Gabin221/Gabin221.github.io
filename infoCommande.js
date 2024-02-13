c671c523435a346ea83fe7726e4e51466b69b628
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-02-13 14:19:30 +0100

    Delete infoCommande.js

--- infoCommande.js
+++
@@ -1,5 +0,0 @@
-// Événement de clic pour afficher/masquer la bulle
-document.getElementById('boutonInfo1').addEventListener('click', function() {
-    var tooltip = document.getElementById('divInfo1');
-    tooltip.style.display = (tooltip.style.display === 'none') ? 'block' : 'none';
-});
