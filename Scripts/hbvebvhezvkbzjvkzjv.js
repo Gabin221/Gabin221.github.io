77b9a0a043ab659844fe466d1b2880277994ecdf
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-02-16 09:27:16 +0100

    Update

--- Scripts/hbvebvhezvkbzjvkzjv.js
+++ Scripts/hbvebvhezvkbzjvkzjv.js
@@ -1,13 +1,9 @@
-import firebase from "firebase/app";
-import "firebase/compat/database";
-
 const firebaseConfig = {
     apiKey: "AIzaSyDMDW_cM2CGBLoHfLr75qXAfIF7w02MI7U",
     authDomain: "gabin-s-bar.firebaseapp.com",
     projectId: "gabin-s-bar",
-    databaseURL: "https://gabin-s-bar.europe-west2.firebasedatabase.app",
     storageBucket: "gabin-s-bar.appspot.com",
     messagingSenderId: "64743999090",
     appId: "1:64743999090:web:41b193b218ec3726f964eb",
     measurementId: "G-Q8MTX3X4JB"
-  };
+};
