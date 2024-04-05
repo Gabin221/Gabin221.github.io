9d077c815359aa93db646b856096de99bf1f9336
Gabin221 <serrurot19@gmail.com>
2024-04-05 15:05:51 +0200

    Update tachyonRecupererCodes.js

--- Scripts/tachyonRecupererCodes.js
+++ Scripts/tachyonRecupererCodes.js
@@ -54,42 +54,43 @@ function fetchDataFromFirestore(path, div, langage) {
             const lines = code.split('\n');
 
             const codeContent = lines.map((line, index) => {
-                if (langage === 'Shell') {
-                    return `<div style='margin-right: 5px;'>${index + 1}<span style='color: blue;'> :~$</span> ${line}</div>`;
-                } else {
-                    return `<div style='margin-right: 5px;'>${index + 1}</div>`;
-                }
+                // if (langage === 'Shell') {
+                //     return `<div style='margin-right: 5px;'>${index + 1}<span style='color: blue;'> :~$</span> ${line}</div>`;
+                // } else {
+                //     return `<div style='margin-right: 5px;'>${index + 1}</div>`;
+                // }
+                return `<div style='margin-right: 5px;'>${index + 1}</div>`;
             }).join('');
 
             const html = `
-<div class='element' style='display: block; height: auto;'>
-    ${nom}<br><br>
-    ${description}<br><br>
-    <div style='background-color: #f5f2f0; color: black; display: flex; justify-content: space-between; align-items: center; margin: 0; border-bottom: 1px solid black;'>
-        <span style='margin-left: 10px; font-size: 16px;'>
-            ${langage}
-        </span>
-        <button id='copyButton${compteur}' onclick='copyCode("codeBlock", this)' style='margin-top: 0; float: right; margin: 10px; padding: 5px 10px; font-size: 16px; box-shadow: true; border: 1px solid black; cursor: pointer;'>
-            <div id='buttonCopier'>
-                Copier
-            </div>
-        </button>
-    </div>
-<div style='line-height: 24px;'>
-<div style='display: flex; font-size: 16px;'>
-<div style='background-color: #f5f2f0; width: 10%; display: flex; align-items: flex-start; justify-content: center; height: 100%; align-self: stretch; padding-top: 16px; padding-bottom: 16px;'>
-<div>
-${codeContent}
-</div>
-</div>
-<pre style='margin-top: 0; width: 90%; height: 100%; float: right; border-left: 1px solid black; display: flex; align-items: flex-start; align-self: stretch;'>
-<code style='font-size: 16px;' class='${langageClasses[langage]}' id='codeBlock' data-language='${langage}'>
+                <div class='element' style='display: block; height: auto;'>
+                    ${nom}<br><br>
+                    ${description}<br><br>
+                    <div style='background-color: #f5f2f0; color: black; display: flex; justify-content: space-between; align-items: center; margin: 0; border-bottom: 1px solid black;'>
+                        <span style='margin-left: 10px; font-size: 16px;'>
+                            ${langage}
+                        </span>
+                        <button id='copyButton' onclick='copyCode("codeBlock", "copyButton")' style='margin-top: 0; float: right; margin: 10px; padding: 5px 10px; font-size: 16px; box-shadow: true; border: 1px solid black; cursor: pointer;'>
+                            <div id='buttonCopier'>
+                                Copier
+                            </div>
+                        </button>
+                    </div>
+                    <div style='line-height: 24px;'>
+                        <div style='display: flex; font-size: 16px;'>
+                            <div style='background-color: #f5f2f0; width: 10%; display: flex; align-items: flex-start; justify-content: center; height: 100%; align-self: stretch; padding-top: 16px; padding-bottom: 16px;'>
+                                <div>
+                                    ${codeContent}
+                                </div>
+                            </div>
+                            <pre style='margin-top: 0; width: 90%; height: 100%; float: right; border-left: 1px solid black; display: flex; align-items: flex-start; align-self: stretch;'>
+                                <code style='font-size: 16px;' class='${langageClasses[langage]}' id='codeBlock' data-language='${langage}'>
 ${code}
-</code>
-</pre>
-</div>
-</div>
-</div>
+                                </code>
+                            </pre>
+                        </div>
+                    </div>
+                </div>
             `;
 
             div.innerHTML += html;
