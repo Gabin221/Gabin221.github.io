eb92d13bcb0ffe4cf7fad48c350224a176644537
Gabin221 <serrurot19@gmail.com>
2024-04-17 12:33:02 +0200

    Update

--- Scripts/moiGraphiquePleinVoiture.js
+++ Scripts/moiGraphiquePleinVoiture.js
@@ -1,13 +1,13 @@
-// Fonction pour récupérer les noms des voitures à partir de la collection "Voitures"
+// Fonction pour récupérer les noms des documents dans la collection "Voitures"
 function fetchCarNames() {
-    db.collection("Voitures").get().then(querySnapshot => {
-        const voitureSelect = document.getElementById("voitureSelect");
-        
+    var voitureSelect = document.getElementById("voitureSelect");
+
+    db.collection("Voitures").get().then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
-            const option = document.createElement("option");
-            option.value = doc.id;
-            option.textContent = doc.id;
-            voitureSelect.appendChild(option);
+            var option = document.createElement("option");
+            // Remplacer le caractère "+" par "plus"
+            option.text = doc.id;
+            voitureSelect.add(option);
         });
     }).catch(function(error) {
         console.error("Erreur lors de la récupération des noms de voiture :", error);
@@ -17,49 +17,57 @@ function fetchCarNames() {
 // Fonction pour récupérer les données des pleins et dessiner le graphique correspondant à la voiture sélectionnée
 function fetchAndDrawChart() {
     var selectedCar = document.getElementById("voitureSelect").value;
-    console.log("Voiture sélectionnée:", selectedCar); // Ajout du message de débogage
 
     var consommations = [];
 
     var pleinCollectionRef = db.collection("Voitures").doc(selectedCar).collection("Pleins");
 
     pleinCollectionRef.get().then(function(querySnapshot) {
-        console.log("Query snapshot:", querySnapshot); // Ajout du message de débogage
         querySnapshot.forEach(function(doc) {
-            console.log("Document data:", doc.data()); // Ajout du message de débogage
             var plein = doc.data();
             var consommation = (plein.volume * 100) / plein.distance;
             consommations.push(consommation);
         });
 
-        drawChart(consommations);
+        // Calculer la moyenne des consommations
+        var moyenne = calculateMean(consommations);
+
+        // Dessiner le graphique avec la moyenne des consommations
+        drawChart(consommations, moyenne);
     }).catch(function(error) {
         console.error("Erreur lors de la récupération des données des pleins :", error);
     });
+}
+
+// Fonction pour calculer la moyenne d'un tableau de valeurs
+function calculateMean(values) {
+    var sum = values.reduce((a, b) => a + b, 0);
+    return sum / values.length;
 }
 
 // Variable globale pour stocker l'instance du graphique
 var myChart = null;
 
 // Fonction pour dessiner le graphique avec Chart.js
-function drawChart(consommations) {
-    var ctx = document.getElementById("myChart").getContext("2d");
-    
-    // Vérifiez si un graphique existe déjà, si oui, détruisez-le
-    if (myChart !== null) {
-        myChart.destroy();
-    }
-
-    myChart = new Chart(ctx, {
-        type: "line",
+function drawChart(consommations, moyenne) {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
         data: {
             labels: Array.from({ length: consommations.length }, (_, i) => i + 1), // Itérations
             datasets: [{
-                label: "Consommation par plein",
+                label: 'Consommation par plein',
                 data: consommations,
-                backgroundColor: "rgba(255, 99, 132, 0.2)",
-                borderColor: "rgba(255, 99, 132, 1)",
+                backgroundColor: '#FF0000',
+                borderColor: '#FF0000',
                 borderWidth: 1
+            }, {
+                type: 'line',
+                label: 'Moyenne (' + Math.round(moyenne*1000)/1000 + ' L/100)',
+                data: Array(consommations.length).fill(moyenne), // Répéter la moyenne pour chaque itération
+                borderColor: '#000000',
+                backgroundColor: '#000000',
+                borderWidth: 1,
             }]
         },
         options: {
@@ -71,7 +79,6 @@ function drawChart(consommations) {
         }
     });
 }
-
 // Appeler la fonction pour récupérer les noms des voitures au chargement de la page
 window.onload = function() {
     fetchCarNames();
