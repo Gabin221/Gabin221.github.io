50ff8e42f7435d3a7041e53b1493c03a31af335e
Gabin221 <serrurot19@gmail.com>
2024-04-19 15:31:32 +0200

    Update

---
+++ Scripts/gabinsBarAffichageConditionnelBoutonModifierCarte.js
@@ -0,0 +1,22 @@
+function checkUserStatus() {
+    // Vérifier si l'utilisateur est connecté
+    var userLoggedIn = localStorage.getItem('userLoggedIn');
+    if (userLoggedIn === "true") {
+        // L'utilisateur est connecté, vérifier son niveau de droits
+        var userRights = localStorage.getItem('userRights');
+        if (userRights === "boss") {
+            // L'utilisateur a le niveau de droits "boss"
+            return true;
+        }
+    }
+    // Si l'utilisateur n'est pas connecté ou n'a pas le niveau de droits "boss", retourner false
+    return false;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    //var carteCed = document.querySelector('.carte h3');
+    if (!checkUserStatus()) {
+        // Si l'utilisateur n'est pas connecté ou n'a pas le niveau de droits "boss", masquez la carte
+        document.getElementById('modifierCarteButton').style.display = "none";
+    }
+});
