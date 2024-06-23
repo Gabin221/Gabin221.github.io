7d9e554ebd153029a466a7d825664704f82c7e6a
Gabin221 <serrurot19@gmail.com>
2024-06-23 20:05:13 +0200

    Update

--- Scripts/moduloAffichageFormulaireAjoutSuppressionAvecDroits.js
+++ Scripts/moduloAffichageFormulaireAjoutSuppressionAvecDroits.js
@@ -1,9 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var userRights = sessionStorage.getItem('userRights');
+function decryptData(data, key) {
+    var bytes = CryptoJS.AES.decrypt(data, key);
+    return bytes.toString(CryptoJS.enc.Utf8);
+}
 
-    if (userRights === "boss"){
-        document.getElementById('ajoutSuppressionCode').style.display = "block";
+document.addEventListener('DOMContentLoaded', function () {
+    var encryptionKey = 'secretKey'; // La même clé utilisée pour chiffrer
+
+    var encryptedUserLoggedIn = sessionStorage.getItem('userLoggedIn');
+    var encryptedUserRights = sessionStorage.getItem('userRights');
+
+    if (encryptedUserLoggedIn && encryptedUserRights) {
+        var userLoggedIn = decryptData(encryptedUserLoggedIn, encryptionKey);
+        var userRights = decryptData(encryptedUserRights, encryptionKey);
+
+        if (userLoggedIn === 'true' && userRights === 'boss') {
+            document.getElementById('ajoutSuppressionCode').style.display = 'block';
+        } else {
+            document.getElementById('ajoutSuppressionCode').style.display = 'none';
+        }
     } else {
-        document.getElementById('ajoutSuppressionCode').style.display = "none";
+        document.getElementById('ajoutSuppressionCode').style.display = 'none';
     }
 });
