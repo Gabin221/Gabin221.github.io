63e4b781d8cbfc3485f73fdd6196b44d124e5880
Gabin221 <serrurot19@gmail.com>
2024-06-24 14:58:02 +0200

    Update

--- Scripts/allPagesConnexionDeconnexion.js
+++ Scripts/allPagesConnexionDeconnexion.js
@@ -1,39 +1,62 @@
+// Fonction de déconnexion
 function logout() {
-    sessionStorage.removeItem('userLoggedIn');
-    sessionStorage.removeItem('currentUser');
-    sessionStorage.removeItem('userRights');
-    window.location.href = '../';
-}
-
-function login() {
-    window.location.href = '../Connexion/index.html';
-}
-
-function loginout() {
-    if (sessionStorage.getItem('userLoggedIn')) {
-        logout();
+    // Supprimer les informations de connexion du stockage local
+    localStorage.removeItem('userLoggedIn');
+    localStorage.removeItem('currentUser');
+    localStorage.removeItem('userRights');
+    // Redirection vers la page précédente ou la page par défaut
+    var previousPage = localStorage.getItem('previousPage');
+    if (previousPage) {
+        window.location.href = previousPage;
     } else {
-        login();
+        window.location.href = 'index.html'; // Page par défaut
     }
 }
 
-function decryptData(data, key) {
-    var bytes = CryptoJS.AES.decrypt(data, key);
-    return bytes.toString(CryptoJS.enc.Utf8);
+// Fonction pour encoder le mot de passe avec SHA-256 en utilisant Forge
+function encryptPassword(password) {
+    var md = forge.md.sha256.create();
+    md.update(password);
+    return md.digest().toHex();
 }
 
-const svgElement = document.getElementById('buttonAccount');
-var encryptionKey = 'secretKey';
-var userLoggedIn = decryptData(sessionStorage.getItem('userLoggedIn'), encryptionKey);
-const connecte = `<svg onCLick="loginout()" id="buttonAccount" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,6A2,2 0 0,0 10,8A2,2 0 0,0 12,10A2,2 0 0,0 14,8A2,2 0 0,0 12,6M12,13C14.67,13 20,14.33 20,17V20H4V17C4,14.33 9.33,13 12,13M12,14.9C9.03,14.9 5.9,16.36 5.9,17V18.1H18.1V17C18.1,16.36 14.97,14.9 12,14.9Z" /></svg>`;
-const nonConnecte = `<svg onCLick="loginout()" id="buttonAccount" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M2.75,7L4.03,5.75L13.26,15L20,21.72L18.73,23L15.73,20H4V17C4,15.14 6.61,13.92 9.09,13.36L2.75,7M20,17V19.18L18.1,17.28V17C18.1,16.74 17.6,16.35 16.8,16L14,13.18C16.71,13.63 20,14.91 20,17M5.9,17V18.1H13.83L10.72,15C8.19,15.3 5.9,16.45 5.9,17M12,4A4,4 0 0,1 16,8C16,9.95 14.6,11.58 12.75,11.93L8.07,7.25C8.42,5.4 10.05,4 12,4M12,6A2,2 0 0,0 10,8A2,2 0 0,0 12,10A2,2 0 0,0 14,8A2,2 0 0,0 12,6Z" /></svg>`;
+// Vérification des informations de connexion
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Empêcher le rechargement de la page
 
-function updateSVG() {
-    if (userLoggedIn === 'true') {
-        svgElement.innerHTML = nonConnecte;
-    } else {
-        svgElement.innerHTML = connecte;
-    }
-}
+    var pseudo = document.getElementById('pseudo').value;
+    var motDePasse = document.getElementById('motDePasse').value;
 
-updateSVG();
+    // Chiffrer le mot de passe saisi par l'utilisateur
+    var hashedPassword = encryptPassword(motDePasse);
+
+    // Vérification des informations de connexion avec Firestore
+    var usersRef = db.collection('user_gabinsbar');
+    usersRef.where('id', '==', pseudo).where('motdepasse', '==', hashedPassword).get()
+        .then((querySnapshot) => {
+            if (!querySnapshot.empty) {
+                // Récupérer les données utilisateur
+                querySnapshot.forEach((doc) => {
+                    var userData = doc.data();
+                    var droits = userData.droits; // Récupérer le niveau de droits de l'utilisateur
+                    // Enregistrement de l'état de connexion de l'utilisateur dans le stockage local ou les cookies, par exemple :
+                    localStorage.setItem('userLoggedIn', true);
+                    localStorage.setItem('currentUser', pseudo);
+                    localStorage.setItem('userRights', droits); // Enregistrer le niveau de droits
+                    // Redirection vers la page précédente ou la page par défaut
+                    var previousPage = localStorage.getItem('previousPage');
+                    if (previousPage) {
+                        window.location.href = previousPage;
+                    } else {
+                        window.location.href = 'index.html'; // Page par défaut
+                    }
+                });
+            } else {
+                // Informer l'utilisateur que les informations de connexion sont incorrectes
+                alert('Les informations de connexion sont incorrectes. Veuillez réessayer.');
+            }
+        })
+        .catch((error) => {
+            console.error('Erreur lors de la vérification des informations de connexion:', error);
+        });
+});
